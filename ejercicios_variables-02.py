25659269cc07fe5a4954fe0341300d72f76d2c0c
Maxi2 <maxhr117@gmail.com>
2025-05-22 22:33:24 -0600

    Agregando segundo ejercicio de variables.

---
+++ ejercicios_variables-02.py
@@ -0,0 +1,22 @@
+# Datos de un vehiculo
+# Dado una fabrica de produccion de vehiculos, se solicita almacenar la informacion de un vehiculo en 3 variables:
+# 1.La marca del vehiculo
+# 2.El modelo del vehiculo
+# 3.El año de fabricacion
+# Finalmente, agregar algunos valores de ejemplo y mandar a imprimir el valor de cada variable.
+
+marca = 'Chevrolet'
+modelo = 'Camaro'
+anio = 2020
+version = 'SS'
+color = 'Gris Nardo'
+motor = 6.2
+
+print('*** Datos del Vehiculo ***')
+print()
+print(f'Marca del auto: {marca}')
+print(f'Modelo: {modelo}')
+print(f'Año :{anio}')
+print(f'En su version: {version}')
+print(f'Lo tenemos en color: {color}')
+print(f'Cuenta con un motor {motor} litros Supercargado.')
